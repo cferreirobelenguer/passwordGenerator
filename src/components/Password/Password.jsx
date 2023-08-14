322c52b66a9a9f5cf20a2357970d480aca3ebd67
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2023-08-14 20:58:01 +0200

    input password

---
+++ src/components/Password/Password.jsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import '../Password/Password.css';
+import {ContentPaste} from '@mui/icons-material'
+
+const Password = () => {
+    const [textToCopy,setTextToCopy]= useState("");
+    return (
+        <section className="password-container">
+            <div className="password-container-section">
+                <h2 className='password-title'>Password Generator</h2>
+                <div className='password-result'>
+                    <span id="result"></span>
+                    <button className="password-btn" id="clipboard" ><ContentPaste></ContentPaste></button>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default Password;
