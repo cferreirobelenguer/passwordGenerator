ad7b00510123d508862db9296f94c98a4ed9257b
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2023-08-15 14:06:22 +0200

    input value

--- src/components/Password/Password.jsx
+++ src/components/Password/Password.jsx
@@ -3,48 +3,92 @@ import '../Password/Password.css';
 import {ContentPaste} from '@mui/icons-material'
 
 const Password = () => {
-    const [textToCopy,setTextToCopy]= useState("");
+    const [number, setNumber] = useState(20);
+    const [isUppercase, setIsUppercase] = useState(true);
+    const [isLowercase, setIsLowercase] = useState(true);
+    const [isNumeric, setIsNumeric] =useState(true);
+    const [isSymbol, setIsSymbol] = useState(true);
+
+    const handleNumber = (event) => {
+        const inputValue = event.target.value;
+        //If the password length exceeds the limit which is 20; the length will default to 20
+        if (inputValue <= 20) {
+            setNumber(inputValue)
+        }
+    }
+
+    const handleUppercase = (event) => {
+        setIsUppercase(event.target.checked)
+    }
+
+    const handleLowercase = (event) => {
+        setIsLowercase(event.target.checked)
+    }
+
+    const handleNumeric = (event) => {
+        setIsNumeric(event.target.checked)
+    }
+
+    const handleSymbol = (event) => {
+        setIsSymbol(event.target.checked)
+    }
+
+    const handleClipboard = () => {
+        //copy password to clipboard
+        const valueSpan = document.getElementById('result').textContent;
+        navigator.clipboard.writeText(valueSpan);
+    }
+
+    const handleClick = () => {
+        console.log("Número ",number)
+        console.log("Uppercase ",isUppercase)
+        console.log("Lowercase" ,isLowercase)
+        console.log("Numeric ",isNumeric)
+        console.log("Symbol ",isSymbol)
+    }   
+
+    
     return (
         <section className="password-container">
             <div className="password-container-section">
                 <h2 className='password-title'>Password Generator</h2>
                 <div className='password-result'>
-                    <span id="result">hola</span>
-                    <button className="password-btn" id="clipboard" onClick={() => {navigator.clipboard.writeText(textToCopy)}} ><ContentPaste></ContentPaste></button>
+                    <span id="result"></span>
+                    <button className="password-btn" id="clipboard" onClick={handleClipboard} ><ContentPaste></ContentPaste></button>
                 </div>
                 <div className='password-inputs'>
                     <div className='password-input'>
                         <label htmlFor="password">
                             Password Length
                         </label>
-                        <input type="number" id="length" min="4" max="20" value="20"></input>
+                        <input type="number" id="length" min="4" max="20" value={number} onChange={handleNumber}></input>
                     </div>
                     <div className='password-input'>
                         <label htmlFor="uppercase">
                             Include uppercase letters
                         </label>
-                        <input type="checkbox" id="uppercase"value="uppercase" checked></input>
+                        <input type="checkbox" id="uppercase" value="uppercase" checked={isUppercase} onChange={handleUppercase}></input>
                     </div>
                     <div className='password-input'>
                         <label htmlFor="lowercase">
                             Include lowercase letters
                         </label>
-                        <input type="checkbox" id="lowercase"value="lowercase" checked></input>
+                        <input type="checkbox" id="lowercase"value="lowercase" checked={isLowercase} onChange={handleLowercase}></input>
                     </div>
                     <div className='password-input'>
                         <label htmlFor="numbers">
                             Include numbers
                         </label>
-                        <input type="checkbox" id="numbers"value="numbers" checked></input>
+                        <input type="checkbox" id="numbers"value="numbers"checked={isNumeric} onChange={handleNumeric}></input>
                     </div>
                     <div className='password-input'>
                         <label htmlFor="symbols">
                             Include symbols
                         </label>
-                        <input type="checkbox" id="symbols" value="symbols" checked></input>
+                        <input type="checkbox" id="symbols" value="symbols" checked={isSymbol} onChange={handleSymbol}></input>
                     </div>
                     <div>
-                        <button className='password-button'>Generate Password</button>
+                        <button className='password-button' onClick={handleClick}>Generate Password</button>
                     </div>
                 </div>
                 
