32d5fbc35d679361a3bdaefe6739ff0f63169692
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2023-08-15 12:16:07 +0200

    css style

--- src/components/Password/Password.jsx
+++ src/components/Password/Password.jsx
@@ -12,6 +12,44 @@ const Password = () => {
                     <span id="result">hola</span>
                     <button className="password-btn" id="clipboard" onClick={() => {navigator.clipboard.writeText(textToCopy)}} ><ContentPaste></ContentPaste></button>
                 </div>
+                <div className='password-inputs'>
+                    <div className='password-input'>
+                        <label htmlFor="password">
+                            Password Length
+                        </label>
+                        <input type="number" id="length" min="4" max="20" value="20"></input>
+                    </div>
+                    <div className='password-input'>
+                        <label htmlFor="uppercase">
+                            Include uppercase letters
+                        </label>
+                        <input type="checkbox" id="uppercase"value="uppercase" checked></input>
+                    </div>
+                    <div className='password-input'>
+                        <label htmlFor="lowercase">
+                            Include lowercase letters
+                        </label>
+                        <input type="checkbox" id="lowercase"value="lowercase" checked></input>
+                    </div>
+                    <div className='password-input'>
+                        <label htmlFor="numbers">
+                            Include numbers
+                        </label>
+                        <input type="checkbox" id="numbers"value="numbers" checked></input>
+                    </div>
+                    <div className='password-input'>
+                        <label htmlFor="symbols">
+                            Include symbols
+                        </label>
+                        <input type="checkbox" id="symbols" value="symbols" checked></input>
+                    </div>
+                    <div>
+                        <button className='password-button'>Generate Password</button>
+                    </div>
+                </div>
+                
+                
+
             </div>
         </section>
     )
